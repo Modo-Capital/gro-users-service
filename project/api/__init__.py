5d9a602674b7c794f470b9da0fb46582520e4bb8
Troy Do <hoangdov@gmail.com>
2018-06-05 13:28:55 -0700

    update

--- project/api/__init__.py
+++ project/api/__init__.py
@@ -11,11 +11,11 @@ from .social_media import api as social_media
 from .gro_scores import api as gro_scores
 from flask import url_for
 
-# @property
-# def specs_url(self):
-# 	return url_for(self.endpoint('specs'), _external=True, _scheme='https')
+@property
+def specs_url(self):
+	return url_for(self.endpoint('specs'), _external=True, _scheme='https')
 
-# Api.specs_url = specs_url
+Api.specs_url = specs_url
 
 ### Define API with restplus.api
 api = Api (

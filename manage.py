b3246635676dc5211db848b9cf92412fbdee9a75
root <root@ip-172-31-24-116.ec2.internal>
2019-02-12 00:10:58 +0000

    modify manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask import Flask, url_for, redirect, render_template, request, abort, jso
 import unittest
 import coverage
 
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from project import create_app, db
@@ -53,7 +53,7 @@ migrate = Migrate(app, db)
 # Manager Configuration
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
+manager.add_command('infinite_run', Server(host='0.0.0.0', port=5000))
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, AdminUser, Role)
 security = Security(app, user_datastore)
@@ -295,6 +295,26 @@ def seed_db():
     db.session.add(User(email="delighted@troy.do", password="12345678", status="registered", admin=True))
     db.session.add(User(email="mcdonald@trump.com", password="skcihcnaissurevol", status="registered", admin=False))
     db.session.commit()
+
+@manager.option('-h', '--host', dest='host', default='0.0.0.0')
+@manager.option('-p', '--port', dest='port', type=int, default=6969)
+@manager.option('-w', '--workers', dest='workers', type=int, default=3)
+def gunicorn(host, port, workers):
+    """Start the Server with Gunicorn"""
+    from gunicorn.app.base import Application
+
+    class FlaskApplication(Application):
+        def init(self, parser, opts, args):
+            return {
+                'bind': '{0}:{1}'.format(host, port),
+                'workers': workers
+            }
+
+        def load(self):
+            return app
+
+    application = FlaskApplication()
+    return application.run()
 
 if __name__ == '__main__':
   manager.run()
